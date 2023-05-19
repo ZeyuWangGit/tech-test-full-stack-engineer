1c93eb1c9d11dc8baa631c865bcf05ea4fd1304c
Zeyu Wang <wzy.franklin@gmail.com>
2023-05-19 16:10:31 +0800

    feat: remove unused code

--- server/src/Hipages.Tradies.Api/Program.cs
+++ server/src/Hipages.Tradies.Api/Program.cs
@@ -9,32 +9,20 @@ Log.Information("TradieApp Starting");
 
 var builder = WebApplication.CreateBuilder(args);
 
-Log.Information("TradieApp Starting - 1");
-
-//builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration
-//    .WriteTo.Console()
-//    .ReadFrom.Configuration(context.Configuration));
-
 builder.Host.UseSerilog((context, services, configuration) => configuration
         .ReadFrom.Configuration(context.Configuration)
         .ReadFrom.Services(services)
         .Enrich.FromLogContext(),
     true);
 
-Log.Information("TradieApp Starting - 2 ");
-
 var app = builder
     .ConfigureServices()
     .ConfigurePipeline();
-
-Log.Information("TradieApp Starting - 3");
 
 app.UseSerilogRequestLogging();
 
 Log.Information("TradieApp Running");
 
 app.Run();
-
-
 
 public partial class Program { }
